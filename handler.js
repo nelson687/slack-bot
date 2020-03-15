210e3a9b8532b968b57ab9453532bef9dc910f92
Nelson Gallardo <gnelson85@gmail.com>
2020-03-15 01:00:59 +0000

    changes

--- handler.js
+++ handler.js
@@ -1,38 +1,59 @@
 'use strict';
 
+const Slack = require('slack');
+
 const https = require('https'),
       qs = require('querystring'),
-      VERIFICATION_TOKEN = "wdyBeIKGCREec97SaqR0r7oJ",
-      ACCESS_TOKEN = "xoxb-401801756722-990284655665-BhTGrRNNhVEl0AHNxuvSjpsz";
+      VERIFICATION_TOKEN = "",
+      ACCESS_TOKEN = "";
 
-function verify(data) {
+function verify(data, callback) {
     if (data.token === VERIFICATION_TOKEN) {
-        return data.challenge;
+        return callback(null, data.challenge);
     }
-    else return "verification failed ffs";
+    else return callback("verification failed ffs");
 }
 
 let response = {
   statusCode: 200,
   headers: {
     'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    'X-Slack-No-Retry': 1,
   },
   body:{},
 };
 
-module.exports.run = (event, context, callback) => {
-  let body = JSON.parse(event.body);
-    
-  switch (body.type) {
-    case "url_verification": response.body = verify(body); break;
-    case "event_callback": 
-        // await process(body.event);
-        response.body = {ok: true};
-        break;
-    default: response.body = JSON.stringify("Bad request"); break;
+// Post message to Slack - https://api.slack.com/methods/chat.postMessage
+function process(event, callback) {
+  if (!event.bot_id && /(listen)/ig.test(event.text)) {
+      var text = `<@${event.user}> I listen to the messages!!`;
+      var message = { 
+          token: ACCESS_TOKEN,
+          channel: event.channel,
+          text: text
+      };
+
+      Slack.chat.postMessage(message);
   }
 
-  // response.body = JSON.stringify("esa nelsoooon")
-
+  response.body = JSON.stringify("ok");
   callback(null, response);
+}
+
+
+module.exports.run = (event, context, callback) => {
+  let body = JSON.parse(event.body);
+  console.log("RUN STARTING!!!!!!")
+  // let body = event.body;
+  console.log(body)
+  switch (body.type) {
+    case "url_verification": response.body = verify(body); break;
+    case "event_callback":
+        process(body.event, callback);
+        break;
+    default: 
+        response.body = JSON.stringify("Bad request");
+        callback(null, response);
+        break;
+  }
 };
